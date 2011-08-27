03529f297a27336abbaffef5e3b8094a098b505a
Vashishtha Jogi <vashishthajogi@gmail.com>
2011-08-27 15:10:18 +0530

    Initial Commit

---
+++ GTFSImporter/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GTFSImporter
+//
+//  Created by Vashishtha Jogi on 8/27/11.
+//  Copyright 2011 Adobe Systems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
