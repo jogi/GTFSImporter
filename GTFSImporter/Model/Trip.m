bd5a1b53c3a03aec4947e663d985075086bf26ca
Kevin Conley <kevindconley@gmail.com>
2013-06-25 22:06:42 -0700

    Customized trip for Stanford (added shape_id)

--- GTFSImporter/Model/Trip.m
+++ GTFSImporter/Model/Trip.m
@@ -23,11 +23,11 @@
 - (id) initWithDB:(FMDatabase *)fmdb
 {
     self = [super init];
-	if (self)
-	{
-		db = fmdb;
-	}
-	return self;
+    if (self)
+    {
+        db = fmdb;
+    }
+    return self;
 }
 
 - (void)addTrip:(Trip *)trip
@@ -39,15 +39,16 @@
             return;
         }
     }
-    
-    [db executeUpdate:@"INSERT into trips(block_id,route_id,direction_id,trip_headsign,service_id,trip_id) values(?, ?, ?, ?, ?, ?)",
+
+    [db executeUpdate:@"INSERT into trips(block_id,route_id,direction_id,trip_headsign,service_id,shape_id,trip_id) values(?, ?, ?, ?, ?, ?, ?)",
      trip.blockId,
      trip.routeId,
      trip.directionId,
      trip.tripHeadsign,
      trip.serviceId,
+     trip.shapeId,
      trip.tripId];
-    
+
     if ([db hadError]) {
         NSLog(@"Err %d: %@", [db lastErrorCode], [db lastErrorMessage]);
         return;
@@ -63,25 +64,25 @@
             return;
         }
     }
-    
+
     //Drop table if it exists
     NSString *drop = @"DROP TABLE IF EXISTS trips";
-    
+
     [db executeUpdate:drop];
-    
+
     if ([db hadError]) {
         NSLog(@"Err %d: %@", [db lastErrorCode], [db lastErrorMessage]);
         return;
     }
-    
+
     //Create table
-    NSString *create = @"CREATE TABLE 'trips' ('block_id' varchar(11) DEFAULT NULL, 'route_id' varchar(11) DEFAULT NULL, 'direction_id' tinyint(1) DEFAULT NULL, 'trip_headsign' varchar(255) DEFAULT NULL, 'service_id' varchar(11) DEFAULT NULL, 'trip_id' varchar(11) NOT NULL, PRIMARY KEY ('trip_id'))";
-    
+    NSString *create = @"CREATE TABLE 'trips' ('block_id' varchar(11) DEFAULT NULL, 'route_id' varchar(11) DEFAULT NULL, 'direction_id' tinyint(1) DEFAULT NULL, 'trip_headsign' varchar(255) DEFAULT NULL, 'service_id' varchar(11) DEFAULT NULL, 'shape_id' varchar(11) DEFAULT NULL, 'trip_id' varchar(11) NOT NULL, PRIMARY KEY ('trip_id'))";
+
     NSString *createIndex = @"CREATE INDEX route_id_trips ON trips(route_id)";
-    
+
     [db executeUpdate:create];
     [db executeUpdate:createIndex];
-    
+
     if ([db hadError]) {
         NSLog(@"Err %d: %@", [db lastErrorCode], [db lastErrorMessage]);
         return;
@@ -96,15 +97,16 @@
     tripRecord.directionId = aRecord[@"direction_id"];
     tripRecord.tripHeadsign = aRecord[@"trip_headsign"];
     tripRecord.serviceId = aRecord[@"service_id"];
+    tripRecord.shapeId = aRecord[@"shape_id"];
     tripRecord.tripId = aRecord[@"trip_id"];
-    
+
     [self addTrip:tripRecord];
 }
 
 - (NSArray *)getAllTripIds
 {
     NSMutableArray *tripIds = [[NSMutableArray alloc] init];
-    
+
     if (db==nil) {
         db = [FMDatabase databaseWithPath:[Util getDatabasePath]];
         if (![db open]) {
@@ -112,12 +114,12 @@
             db = nil;
             return nil;
         }
-        
+
         db.shouldCacheStatements=YES;
     }
-    
+
     NSString *query = @"SELECT trip_id from trips";
-    
+
     FMResultSet *rs = [db executeQuery:query];
     while ([rs next]) {
         [tripIds addObject:[rs objectForColumnName:@"trip_id"]];
@@ -125,7 +127,7 @@
     // close the result set.
     [rs close];
     [db close];
-    
+
     //    NSLog(@"getStopTimesByTripId %d", [stop_times count]);
     return tripIds;
 }
